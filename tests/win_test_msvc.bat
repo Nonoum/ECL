41068040d91ccee7790778a7c13b2cbaa81cdf25
nnm <evgeniy.evstratov@gmail.com>
2018-01-25 22:19:21 +0200

    Compile as C for running tests

--- tests/win_test_msvc.bat
+++ tests/win_test_msvc.bat
@@ -1,3 +1,3 @@
-set "compile=cl tests.cpp /O2 /EHsc /DECL_USE_ASSERT /DECL_USE_BITNESS"
+set "compile=cl tests.cpp all_c.c /O2 /EHsc /DECL_BUILD_AS_C /DECL_USE_ASSERT /DECL_USE_BITNESS"
 set "run_tests=tests.exe"
 del tests.exe && %compile%_16 && %run_tests% && %compile%_32 && %run_tests% && %compile%_64 && %run_tests%
