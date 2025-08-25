db595127ed16ae518fe1f3e0ea8ed7561922c58d
Nonoum <evgeniy.evstratov@gmail.com>
2025-08-25 15:40:15 +0300

    Add Huff8 sample (blocked, user-specified block size)

---
+++ sample/sample_huff8_ulm_blocked.cpp
@@ -0,0 +1,222 @@
+#include "../ECL_common.c"
+#include "../ECL_Huff8.c"
+#include "../ECL_utils.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <cassert>
+
+void s_show_usage() {
+    std::cout << "-- Usage compress: sample c 32768 my-src-file my-output-compressed-file" << std::endl;
+    std::cout << "-- Usage decompress: sample d my-compressed-file my-output-file" << std::endl;
+    std::cout << "  where '32768' is block_size for Huffman processing, which has to be > 0 and < 65536" << std::endl;
+    std::cout << "  e.g:" << std::endl;
+    std::cout << "    sample c 20 111.txt 111.txt.huff8" << std::endl;
+    std::cout << "    sample d 111.txt.huff8 111.txt.recovered" << std::endl;
+    std::cout << "  will provide '111.txt.huff8' compressed file, and '111.txt.recovered' decompressed file matching original '111.txt'" << std::endl;
+    std::cout << "  OPERANDS ORDER IS STRICT." << std::endl;
+}
+
+typedef std::vector<uint8_t> Raw;
+
+bool s_file_exists(const char* fname) {
+    std::ifstream ifs(fname, std::ios::binary);
+    return !!ifs;
+}
+
+Raw s_read_file(const char* fname) {
+    std::ifstream ifs(fname, std::ios::binary);
+    if(! ifs) {
+        return Raw();
+    }
+    ifs.seekg(0, ifs.end);
+    auto size = ifs.tellg();
+    ifs.seekg(0, ifs.beg);
+    if(size) {
+        Raw data;
+        data.resize(size);
+        if(ifs.read((char*)data.data(), size)) {
+            return data;
+        }
+    }
+    return Raw();
+}
+
+Raw s_handle_src(const char* src_fname) {
+    if(! s_file_exists(src_fname)) {
+        std::cout << "- error: file '" << src_fname << "' doesn't exist / can't be opened" << std::endl;
+        return Raw();
+    }
+    auto src = s_read_file(src_fname);
+    if(! src.size()) {
+        std::cout << "- error: can't read file '" << src_fname << "' or it's empty" << std::endl;
+        return Raw();
+    }
+    return src;
+}
+
+bool s_write_file_part(std::ostream& file, const uint8_t* data, size_t size) {
+    if(data && size) {
+        if(! file.write((const char*)data, size)) {
+            std::cout << "- error: can't write file" << std::endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool s_write_file(const char* fname, const uint8_t* hdr, size_t hdr_size, const Raw& data) {
+    std::ofstream ofs(fname, std::ios::binary);
+    if(! ofs) {
+        std::cout << "- error: file '" << fname << "' can't be written" << std::endl;
+        return false;
+    }
+    return s_write_file_part(ofs, hdr, hdr_size)
+        && s_write_file_part(ofs, data.data(), data.size());
+}
+
+bool s_try_compress(const char* src_fname, const char* dst_fname, int block_size) {
+    assert(block_size > 0);
+    std::cout << "compressing '" << src_fname << "' to '" << dst_fname << "' with block_size=" << block_size << std::endl;
+    const Raw src = s_handle_src(src_fname);
+    if(! src.size()) {
+        return false;
+    }
+    Raw output;
+    const auto enough_size = ECL_HUFF8_GET_BOUND(src.size()); // ~roughly enough size, we do block-wise compression and add extra markers
+    output.reserve(enough_size); // by fact it's just a little optimization as we'll append data block-by-block, ensure we won't crash on allocation in the end for large file
+    /// SAMPLE SPECIFIC PART START /// fill output block-by-block
+    {
+        const uint16_t size = uint16_t(block_size);
+        Raw tmp_comp_block;
+        // analyze/compress buffers
+        uint32_t buf1024[256];
+        uint8_t buf256[256];
+        uint8_t buf768[768];
+
+        for(size_t start = 0; start < src.size(); ) {
+            auto portion_size = std::min<size_t>((src.size() - start), size_t(block_size));
+            auto comp_alloc_size = ECL_HUFF8_GET_BOUND(portion_size);
+            assert(portion_size);
+
+            const size_t prefix_size = 2; // uint16_t 'current block size' aka number of bytes in current input
+            tmp_comp_block.resize( comp_alloc_size + prefix_size );
+            tmp_comp_block[0] = uint8_t(portion_size & 0xFF);
+            tmp_comp_block[1] = uint8_t((portion_size >> 8) & 0xFF);
+
+            auto comp_dst = tmp_comp_block.data() + prefix_size;
+            const auto csize = ECL_Huff8_Compress16_ULM_Raw(src.data() + start, uint16_t(portion_size), 1, buf1024, buf256, buf768, comp_dst, comp_alloc_size);
+            if(! csize) {
+                std::cout << "- error: unexpected compression error :|" << std::endl;
+                return false;
+            }
+            const auto comp_block_size = prefix_size + ((csize + 7) / 8);
+
+            auto dst_start_index = output.size();
+            output.resize(output.size() + comp_block_size);
+            memcpy(output.data() + dst_start_index, tmp_comp_block.data(), comp_block_size);
+
+            start += portion_size;
+        }
+    }
+    /// SAMPLE SPECIFIC PART END   ///
+    const auto comp_size = output.size();
+    // encode original file size in header - encode as E7 number
+    uint8_t hdr[10];
+    const auto hdr_end = ECL_Helper_WriteE7(hdr, sizeof(hdr), src.size());
+    if(! hdr_end) {
+        std::cout << "- error: unknown stream error :|" << std::endl;
+        return false;
+    }
+    const auto hdr_size = hdr_end - hdr;
+    const auto total_size = comp_size + hdr_size;
+    // write file data
+    std::cout << "- successfully compressed: original size = " << src.size() << std::endl;
+    std::cout << "compressed stream size = " << comp_size
+              << " (with " << hdr_size << " byte header = " << total_size << ")" << std::endl;
+    std::cout << "ratio = " << std::fixed << (double(comp_size) / double(src.size())) << std::endl;
+    return s_write_file(dst_fname, hdr, hdr_size, output);
+}
+
+bool s_try_decompress(const char* src_fname, const char* dst_fname) {
+    std::cout << "decompressing '" << src_fname << "' to '" << dst_fname << "'" << std::endl;
+    const Raw src = s_handle_src(src_fname);
+    if(! src.size()) {
+        return false;
+    }
+    ECL_usize original_size;
+    const auto hdr_end = ECL_Helper_ReadE7(src.data(), src.size(), &original_size);
+    if(! hdr_end) {
+        std::cout << "- error: invalid file content" << std::endl;
+        return false;
+    }
+    const auto comp_start = hdr_end;
+    const auto comp_end = src.data() + src.size();
+    Raw recovered;
+    Raw tmp_output;
+    uint8_t buf1025[1025];
+    recovered.reserve(original_size);
+    /// SAMPLE SPECIFIC PART START /// fill output block-by-block
+    for(auto start_ptr = comp_start; start_ptr != comp_end; ) {
+        const size_t prefix_size = 2; // uint16_t 'current block size' aka number of bytes in current block
+        if((comp_end - start_ptr) < prefix_size) {
+            std::cout << "- file error: can't read block size" << std::endl;
+            break; // error
+        }
+        auto portion_size = uint16_t(uint16_t(start_ptr[0]) | (uint16_t(start_ptr[1]) << 8));
+        if(! portion_size) {
+            std::cout << "- file error: zero block size" << std::endl;
+            break; // error
+        }
+        start_ptr += prefix_size;
+
+        tmp_output.resize(portion_size);
+        auto left_size = comp_end - start_ptr;
+        auto consumed_size = ECL_Huff8_Decompress_Raw(start_ptr, left_size, buf1025, tmp_output.data(), portion_size, 1);
+        assert(consumed_size);
+        if(consumed_size > left_size) {
+            assert(false);
+            break; // hard error, supposed to be handled inside ECL_Huff8_Decompress_Raw
+        }
+
+        // append decompressed block to output
+        auto dst_start_index = recovered.size();
+        recovered.resize(recovered.size() + portion_size);
+        memcpy(recovered.data() + dst_start_index, tmp_output.data(), portion_size);
+
+        start_ptr += consumed_size;
+    }
+    /// SAMPLE SPECIFIC PART END   ///
+    if(recovered.size() != original_size) {
+        std::cout << "- error: decompression failed - invalid file content" << std::endl;
+        return false;
+    }
+    std::cout << "- successfully decompressed: size = " << original_size << std::endl;
+    return s_write_file(dst_fname, nullptr, 0, recovered);
+}
+
+bool s_cmp_operand(const char* str, const char* expected) {
+    return str && expected && (0 == strcmp(str, expected));
+}
+
+int main(int argc, char* argv[]) {
+    std::cout << "*** Sample ECL program to compress/decompress with ECL:Huff8 (with data split in blocks) ***" << std::endl;
+    if((argc == 4) && s_cmp_operand(argv[1], "d")) {
+        if(s_try_decompress(argv[2], argv[3])) {
+            return 0;
+        }
+        std::cout << std::endl;
+    } else if((argc == 5) && s_cmp_operand(argv[1], "c")) {
+        int block_size = atoi(argv[2]);
+        if((block_size < 1) || (block_size >= 65536)) {
+            std::cout << "- error: block_size has to be > 0 and < 65536" << std::endl;
+        } else if(s_try_compress(argv[3], argv[4], block_size)) {
+            return 0;
+        }
+        std::cout << std::endl;
+    }
+    s_show_usage();
+    return 0;
+}
---
+++ sample/win_msvc__huff8_ulm_blocked.bat
@@ -0,0 +1,1 @@
+cl sample_huff8_ulm_blocked.cpp /O2 /EHsc /Fe:huff8.exe
